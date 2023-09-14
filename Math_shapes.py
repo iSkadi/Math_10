1e2be281de77269896f2e734a33e7bfdf769338c
Trayana Pateva <88454798+iSkadi@users.noreply.github.com>
2023-09-14 11:51:45 +0300

    Add files via upload

---
+++ Math_shapes.py
@@ -0,0 +1,64 @@
+import random
+
+from art import tprint
+
+
+class Shapes:
+    TOTAL_PROBLEMS = 10
+    shapes = ["square", "rectangle", "triangle", "square1", "rectangle1", "triangle1"]
+
+    def generate_problem(self):
+        a = random.randint(1, 10)
+        b = random.randint(1, 10)
+        c = random.randint(1, 10)
+
+
+        shape = random.choice(self.shapes)
+        expr = ""
+        answer = ""
+        if shape == "rectangle":
+            expr = f"На колко е равна обиколката на правоъгълник със страни а = {a} см и b = {b} см?"
+
+            real_answer = str((a + b) * 2)
+            answer = eval(real_answer)
+        elif shape == "rectangle1":
+            d = str((a + b) * 2)
+            expr = f"Обиколката на правоъгълник е {d} см. Едната му страна е {a} см. Колко сантиметра е дълга другата му страна? "
+            real_answer = str(b)
+            answer = eval(real_answer)
+
+        elif shape == "square":
+            expr = f"На колко е равна обиколката на квадрат със страна а = {a} см? "
+            real_answer = str((a * 4))
+            answer = eval(real_answer)
+        elif shape == "square1":
+            d = str((a * 4))
+            expr = f"Обиколката на квадрат е {d} см. Колко сантиметра е дълга страна му? "
+            real_answer = str(a)
+            answer = eval(real_answer)
+        elif shape == "triangle":
+            expr = f"На колко е равна обиколката на триъгълник със страни а = {a} см, b = {b} см и c = {c} см?"
+            real_answer = str((a + b + c))
+            answer = eval(real_answer)
+        elif shape == "triangle1":
+            d = str((a + b + c))
+            expr = f"Обиколката на триъгълник е {d} см. Едната му страна е {a} см, другата {b} см. Колко сантиметра е дълга третата му страна? "
+            real_answer = str(c)
+            answer = eval(real_answer)
+
+        return expr, answer
+
+    def Range(self):
+        for i in range(self.TOTAL_PROBLEMS):
+            expr, answer = self.generate_problem()
+            while True:
+                guess = input("Задача № " + str(i + 1) + ': ' + expr + "  Отговор: ")
+                if guess == str(answer):
+                    break
+
+
+My_object = Shapes()
+My_object.generate_problem()
+My_object.Range()
+
+tprint("You Win!", font="xlarge")
